085edf968e77ab2a0f64b7a243db5828d47e9d95
azabroflovski <azabroflovski@gmail.com>
2022-10-01 16:18:00 +0500

    fix(pwa): manifest title

--- vite.config.ts
+++ vite.config.ts
@@ -97,8 +97,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'azabroflovski',
+        short_name: 'azabroflovski',
         theme_color: '#ffffff',
         icons: [
           {
